9f244596f4f337b6d24d794425b3c2a67fb4619b
canhsan <1979hasancan@gmail.com>
2023-03-08 13:35:24 +0300

    İkinci Versiyon

--- src/gölbaşı/burası.java
+++ src/gölbaşı/burası.java
@@ -3,6 +3,7 @@ package gölbaşı;
 public class burası {
     public static void main(String[] args) {
         System.out.println("Lütfen bir okul adı giriniz");
+        System.out.println("Lütfen bir şehir adı giriniz");
 
     }
 }
